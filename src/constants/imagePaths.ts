d48e707470d0366d11d6fda8bc5d1d6b6b84f0fb
장한울 <swingjhw@naver.com>
2023-05-06 23:48:58 +0900

    feat(imagePaths): 이미지 경로 객체 생성

---
+++ src/constants/imagePaths.ts
@@ -0,0 +1,25 @@
+interface IImagePaths {
+    [key:string]: IObject
+}
+interface IObject {
+    [key:string]:string
+}
+
+
+const imagePaths:IImagePaths = {
+    foodCategory: {
+        koreanFood: './category-korean.png',
+        japaneseFood: './category-japanese.png',
+        chineseFood: './category-chinese.png',
+        asianFood: './category-asian.png',
+        westernFood: './category-western.png',
+        etcFood: './category-etc.png',
+    },
+    isLike: {
+        like: './favorite-icon-filled.png',
+        notLike: './favorite-icon-lined.png',
+    },
+    generalIcon: {
+        addButton: './add-button.png'
+    }
+}
