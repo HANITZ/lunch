3fa2c3b4594a2343508de22ffed3ed2bb777df2e
장한울 <swingjhw@naver.com>
2023-05-06 23:10:40 +0900

    feat: header, main 구현

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,22 @@
+import navBar from "./js/navBar"
 
 
 
 const App = (el: HTMLElement) => {
     
+    
     const start = () => {
-        
+        const header = document.createElement('header')
+        header.className = 'gnb'
+        el.appendChild(header)
+
+        const main = document.createElement('main')
+        el.appendChild(main)
+
     }
-    const renderTotalHTML = () => {
-        el.innerHTML = 
-        `<header class="gnb">
-        </header>
-        <main>
-        <section class="restaurant-filter-container"></section>
-        <section class="restaurant-list-container"></section>
-        <div class="modal modal--open"></div>
-        </main>
-        `
-    }
+
+    start()
+
     return{
         start
     }
---
+++ src/js/navBar.ts
@@ -0,0 +1,18 @@
+
+
+
+const navBar = (el: HTMLElement) => {
+
+
+    
+    const newEl = document.createElement('header')
+    newEl.className = 'gnb'
+    console.log(newEl)
+
+
+
+    return newEl.outerHTML
+
+}
+
+export default navBar;
