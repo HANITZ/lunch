e9fd183987f6e21ecf85d791c679bc9c2a329cd3
장한울 <swingjhw@naver.com>
2023-05-07 04:42:35 +0900

    refactor: createElement로 변경

--- src/App.ts
+++ src/App.ts
@@ -6,11 +6,9 @@ import createElement from "./util/createElement"
 
 const App = (app: HTMLElement) => {
     
-    // const header = document.createElement('header')
-    // header.className = 'gnb'
-    // app.appendChild(header)
-    const header = createElement('header', {'class': 'gnb'}, app)
-    const main = createElement('main', {}, app)
+
+    const header = createElement('header', {'class': 'gnb'}, app, '')
+    const main = createElement('main', {}, app, '')
 
 
 
@@ -19,24 +17,20 @@ const App = (app: HTMLElement) => {
     const start = () => {
 
         
+        const headerH1 = createElement('h1', {
+            'class': 'gnb__title text-title',
+        }, header, '점심 뭐 먹지')
         
-        
+        const headerButton = createElement('button', {
+            'class': 'gnb__button',
+            'aria-label': '음식점 추가'
+        }, header, '')
 
-        const headerH1 = document.createElement('h1')
-        headerH1.className = 'gnb__title text-title'
-        headerH1.innerText = '점심 뭐 먹지'
-        header.appendChild(headerH1)
 
-        const headerButton = document.createElement('button')
-        headerButton.type = 'button'
-        headerButton.className = 'gnb__button'
-        headerButton.ariaLabel = '음식점 추가'
-        header.appendChild(headerButton)
+        const headerButtonImg = createElement('img', {
+            'src': `${imagePaths.generalIcon.addButton}`
+        }, headerButton, '')
 
-        const headerButtonImg = document.createElement('img')
-        headerButtonImg.src = imagePaths.generalIcon.addButton
-        headerButtonImg.alt = '음식점 추가'
-        headerButton.appendChild(headerButtonImg)
 
 
         
--- src/util/createElement.ts
+++ src/util/createElement.ts
@@ -4,10 +4,13 @@ interface IProps {
     [key:string] : string
 }
 
-const createElement = (tag:string, props:IProps, parent:HTMLElement):HTMLElement => {
+const createElement = (tag:string, props:IProps, parent:HTMLElement, content:string):HTMLElement => {
     const element = document.createElement(tag)
     Object.entries(props).forEach(([name, value]) => element.setAttribute(name, value))
     parent.appendChild(element)
+    if (content){
+        element.textContent = content;
+    }
     return element;
 }
 
