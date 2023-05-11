484ccf5d832b8d4f0fe1896f4556109c4c1fd5a4
장한울 <swingjhw@naver.com>
2023-05-12 01:35:45 +0900

    tmp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import '../templates/style.css'
+import '../templates/style.css';
 import App from './App';
-
+import './images/add-button.png';
+import './images/category-korean.png';
+import './images/category-japanese.png';
+import './images/category-chinese.png';
+import './images/category-western.png';
+import './images/category-asian.png';
+import './images/category-etc.png';
+import './images/favorite-icon-filled.png';
+import './images/favorite-icon-lined.png';
 
 App(document.querySelector('#app'));
-
